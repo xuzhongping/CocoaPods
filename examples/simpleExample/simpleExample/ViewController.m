1b17ed7208ee7458042faff89b4de4d0ab1268be
nakahira <1021057927@qq.com>
2021-11-14 21:37:41 +0800

    add simpleExample

---
+++ examples/simpleExample/simpleExample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  simpleExample
+//
+//  Created by 徐仲平 on 2021/11/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
